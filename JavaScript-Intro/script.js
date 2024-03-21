7668e3252857d24e3ff948ce995f43a458c14003
nimely111 <samuelehnimely@gmail.com>
2024-03-21 17:21:37 +0000

    intro to javascript course

---
+++ JavaScript-Intro/script.js
@@ -0,0 +1,88 @@
+/*** INTRODUCTION TO JAVASCRIPT  AND BASIC TOPIC TO BE COVER*/
+// Installing of nodejs https://nodejs.org
+// Javascript Documentation w3 schools / MDN official site for JS 
+//what can you do with Javascript programming language
+/**
+ * JAVASCRIPT REACT / VUE ANGULAR react native - mobile
+ * desktop -- electrom 
+ */
+// Where to place javascript in your code and why
+
+//  TYPES OF COMMENTS
+// double forward slashes --- short line / single line comment
+
+/**
+ * a multi line comment
+ * 
+ * 
+ * dfjgjg
+ * dkgkg
+ * 
+ */
+
+
+// Javascript build-in types
+// A type is a particular data structure
+// JS has 8 build-in types 
+// 7 primitives and 1 object type 
+//Primitive types represents a single immutable value - this means once it's set it can be change. value becomes read only
+//you can create another value based on the existing one in memory
+
+
+let number  = 10; // this is an assignment operator (=) 
+// let number = 5;
+console.log(number)
+// num variable
+// a variable is a container that holds a value
+
+/**
+ */
+// DATA TYPES 
+/**
+ * 1. string - is a sequence of characters used to represent text
+ *      you can use either a single or double quote. ie. 'text' or "text"
+ * 2. Number - is the only numeric type in javascript
+ * 3. Bolean - can only have 2 values: true/false - on / off - yes / no
+ * 4. Undefined - signifies that no value has ever been set 1 value (undefined)
+ * 5. Null - signifies the lack of value or lack of definition
+ * 6. Symbol - is new to ES6 (releasee 2015 - ECMAScript 6) and it widely 
+ *      use in react and other js frameworks like vue, and angular  (
+ *       Not cover in this class )
+ * 7. Arrays - This is a datatype that holds multiple values with in a 
+ *      single variable. It's known for its zero based indexing
+ * 
+ * Object Type
+ * Object - is a collection of key value pairs
+ * 
+ * Variables
+ * There are 3 ways to declare a variable in JS
+ *  1. Var --- ES5 
+ *  2. let --- ES6
+ *  3. Const -- ES6 recomended constant
+ * 
+ * 
+ * ====== COMMON LANGUAGE CONSTRUCTS
+ *  STRING CONCATENATION
+ * BASIC MATH OPERATORS WITH NUMBERS
+ * CONDITIONALS AND EQUALITIES
+ * ARRAYS
+ * OBJECTS
+ *  
+ * 
+ * 
+ * browser -- clien ISP (Internet service provider) ---  DNS (dOMAIN NAME SERVER)
+ */
+
+
+
+// using
+let firstName = 'Samuel'; // string
+let lastName = "Nimely";
+let myContact = '0777973774'
+let myBook = 'Book';
+
+// let y = '14 street'
+// let x = '14 street'
+console.log(myContact, location)
+// console.log(lastName)
+
