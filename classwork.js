6f9c0b013e52c716d1bd364b50970736b92fc8f0
nimely111 <samuelehnimely@gmail.com>
2024-03-28 22:11:16 +0000

    classwork file for students

---
+++ classwork.js
@@ -0,0 +1,22 @@
+//using snake case naming convention
+const first_name = 'John ';
+const middle_name = 'S. ';
+const last_name = 'Doe ';
+const age = 20
+const my_location = 'Old  Road ';
+
+
+//ES5 string concatenation 2014
+//var
+// console.log('Hello my  name is ' + first_name +  middle_name + last_name + 'and I am ' + age + ' years old ' + 'and i live at ' + my_location);
+
+// ES6 template literal(backticks) ECMAScript version 6
+//2015 let const
+console.log(`Hello my name is ${first_name} ${middle_name} ${last_name} and I am ${age} years old, I live at ${my_location}`);
+
+
+
+
+
+
+
