e83da2de937dd28a36f6380177420335f3385df7
nimely111 <samuelehnimely@gmail.com>
2024-03-28 22:12:01 +0000

    intro to strings method in js

---
+++ string_method.js
@@ -0,0 +1,9 @@
+// string methds
+const myText = 'Rhotel Innovation House ';
+console.log(myText.length); //property
+console.log(myText.toUpperCase());//methods
+console.log(myText.toLowerCase());
+console.log(myText.split(''));
+console.log(myText.substring(0, 6));
+console.log(myText.substring(6, 17));
+
