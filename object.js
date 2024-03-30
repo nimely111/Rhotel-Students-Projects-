fbbc265028b0c81ada3eab9f4380c1011d7eed80
nimely111 <samuelehnimely@gmail.com>
2024-03-30 13:47:29 +0000

    using object in javascript

---
+++ object.js
@@ -0,0 +1,44 @@
+//object constructor
+// const myCar = new Object()
+// class myCar
+
+// object literal
+// functional, oop object oriented programming, 
+
+//object is a collection of key/value pairs.
+// the key/value pair can be referred to as property and value
+const person = {
+    //key:value
+    //property:value
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'coding', 'sports', 'movies'],
+    address:{
+        street: '14 Sinkor',
+        city: 'Monrovia',
+        states: 'Montserrado',
+        sibling: {
+            brother: 'Peter',
+            sister: 'Esther'
+        }
+    }
+}
+
+// console.log(person);
+
+//es5  dot notation / bracket notation
+// console.log(`Hello my name is ${person.firstName} ${person.lastName}`);
+// console.log(person.firstName, person.lastName);
+// console.log(person.hobbies);
+// console.log(person.hobbies[1]);
+// console.log(person.address.city);
+console.log(person.address.sibling.sister);
+// modern syntax es6 destructuring
+
+
+const { address: { sibling: {sister} }} = person;
+console.log(sister);
+
+
+
